6ef18c1739ca5fa2bc1ff59b2ba6a723b5706e2b
Azure Chang <azureimf@hotmail.com>
2020-02-24 20:29:11 +0900

    add Coronavirus Simulator

---
+++ CoronaCalc/CoronaCalc/Corona.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoronaCalc
+{
+    class GovernmentParameter
+    {
+        public int bed_count = 20000;
+
+    }
+
+    class CoronaParameter
+    {
+        public double R0_min;
+        public double R0_max;
+        public double org_dead_rate;
+        public double dead_rate;
+        public double dead_rate_no_bed;
+        public double serious_rate;
+        public double no_symptoms_rate;
+        public double no_symptoms_period;
+        public double checkup_rate;
+        public double checkup_symptoms_threshold;
+        public int left_bed_count;
+        public int org_virus_id;
+
+        public CoronaParameter()
+        {
+            R0_min = 3.9;
+            R0_max = 6.6;
+            dead_rate = 0.05;
+            dead_rate_no_bed = 0.11;
+            org_dead_rate = 0.05;
+            serious_rate = 0.25;
+            no_symptoms_rate = 0.5;
+            no_symptoms_period = 14;
+            checkup_rate = 0.05;
+            checkup_symptoms_threshold = 0.8;
+            left_bed_count = 20000;
+
+        }
+
+    }
+
+    class CoronaPatient
+    {
+        public int id;
+        public int family_id;
+        public int source_id;
+        public int family_number;
+        public int age;
+        public int gender;
+        public int use_mask;
+        public int sick_period;
+        public int checkup;
+        public int checkup_relate;
+        public int infected_number;
+        public int contact_number;
+        public int day;
+        public int care_level;
+        public int day_to_dead;
+        public int dead;
+        public int complete_cure;
+        public double activitiy_factor;
+        public double train_fator;
+        public double room_factor;
+        public double health_factor;
+        public double serious_factor;
+
+        public CoronaPatient(Random r, CoronaParameter c)
+        {
+            id = 0;
+            family_id = 0;
+            source_id = 0;
+            checkup = 0;
+            infected_number = 0;
+            day = 0;
+            day_to_dead = 0;
+            dead = 0;
+            complete_cure = 0;
+            age = r.Next(100);
+            gender = r.Next(2);
+            family_number = r.Next(1, 6);
+            care_level = r.Next(1, 6); //1 =何もしない 2=クリニック 3=呼吸器内科 4=入院 5=確診入院
+            checkup_relate = r.Next(2);
+            use_mask = r.Next(2); // 0=未着用 1=着用
+            sick_period = r.Next(7, 14);
+            contact_number = r.Next(0, 70);
+            activitiy_factor = r.NextDouble();
+            train_fator = r.NextDouble();
+            room_factor = r.NextDouble();
+            serious_factor = r.NextDouble();
+            health_factor = r.NextDouble();
+
+            double dead_rate_int = (int)(c.dead_rate * 100);
+            int destiny_dice = r.Next(0, 100);
+
+            if (dice(r, c.dead_rate) == 1)
+                day_to_dead = sick_period;
+        }
+
+        public int dice(Random r, double hit_rate)
+        {
+            hit_rate = (int)(hit_rate * 100);
+            int destiny_dice = r.Next(0, 100);
+
+            if (destiny_dice <= hit_rate)
+                return 1;
+            else
+                return 0;
+        }
+
+        public List<CoronaPatient> infect(Random r, CoronaParameter c)
+        {
+            List<CoronaPatient> infect_list = new List<CoronaPatient>();
+
+            if (this.care_level < 4)
+            {
+                for (int i = 0; i < contact_number; i++)
+                {
+
+                    CoronaPatient contact_p = new CoronaPatient(r, c);
+                    if(contact_p.health_factor < 0.5) { contact_p.health_factor += 0.5; }
+                    contact_p.care_level = 1; //感染されたばかりではなにもしない
+                    contact_p.serious_factor = 0.1; //発症したばかりではひどくない
+
+                    double infect_rate = activitiy_factor * train_fator * room_factor * serious_factor * (1.0 - use_mask) * (1.0 - contact_p.health_factor) * (1.0 - contact_p.use_mask);
+
+                    if (dice(r, infect_rate) == 1)
+                    {
+                        infected_number++;
+                        infect_list.Add(contact_p);
+                    }
+;
+
+                }
+
+                for (int i = 0; i < family_number; i++)
+                {
+                    CoronaPatient family_p = new CoronaPatient(r, c);
+                    family_p.family_id = id;
+                    family_p.source_id = id;
+                    family_p.care_level = 1; //感染されたばかりではなにもしない
+                    family_p.serious_factor = 0.1; //発症したばかりではひどくない
+                    if (family_p.health_factor < 0.5) { family_p.health_factor += 0.5; }
+
+                    double family_infect_rate = room_factor * 2 * serious_factor * (1.0 - family_p.health_factor);
+
+                    if (dice(r, family_infect_rate) == 1)
+                    {
+                        infected_number++;
+                        infect_list.Add(family_p);
+
+                    }
+                }
+            }
+
+            this.day++;
+
+            return infect_list;
+        }
+
+        public int sick_step(Random r, CoronaParameter c)
+        {
+
+            if (this.dead == 0 && day_to_dead > 0)
+            {
+
+                double care_factor = 1.0 / this.care_level; //無視すれば一番進む
+                double age_factor = (age * age) / 10000.0; //年寄り程危ない、指数的に
+                double health_factor_2 = (health_factor - 0.5) * -0.2;
+                double sick_step_factor = (age_factor * care_factor) + health_factor_2 + ((r.NextDouble() - 0.5) * 0.1);
+                this.serious_factor += sick_step_factor;
+
+
+                //1 =何もしない 2=クリニック 3=呼吸器内科 4=入院 5=確診入院
+                if (serious_factor > 0.8 && c.left_bed_count > 0)
+                {
+                    care_level = 5;
+                    serious_factor -= 0.1;
+                }
+                else if (serious_factor > 0.6 && serious_factor <= 0.8 && care_level < 4 && c.left_bed_count > 0)
+                {
+                    care_level = 4;
+                    
+                }
+                else if (serious_factor > 0.4 && serious_factor <= 0.6 && care_level < 3)
+                {
+                    care_level = 3;
+                    use_mask = 1;
+                    contact_number = (int)(contact_number * 0.4);
+                    activitiy_factor = activitiy_factor * 0.4;
+                }
+                else if (serious_factor > 0.2 && serious_factor <= 0.4)
+                {
+                    care_level = 2;
+                    use_mask = dice(r, 0.6); //クリニックにいったら6割はマスク着用
+                    contact_number = (int)(contact_number * 0.6);
+                    activitiy_factor = activitiy_factor * 0.6;
+                }
+
+                if (day >= day_to_dead)
+                    dead = 1;
+            }
+
+            return dead;
+
+            
+        }
+
+        public int complete_cure_step()
+        {
+            if(dead == 0 && day_to_dead == 0)
+            {
+                if (day >= sick_period)
+                    complete_cure = 1;
+            }
+
+            return complete_cure;
+        }
+    }
+}
---
+++ CoronaCalc/CoronaCalc/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoronaCalc
+{
+    class Program
+    {
+        static public CoronaParameter c = new CoronaParameter();
+        static public GovernmentParameter g = new GovernmentParameter();
+        static public List<CoronaPatient> p_list = new List<CoronaPatient>();
+        static public System.Random r = new System.Random();
+
+        static public int day = 0;
+        static public int dead_count = 0;
+        static public int complete_cure_count = 0;
+        static public double mask_step_rate = 1.02;
+        static public double contact_step_rate = 0.98;
+        static public int yesterday_mask_sum = 0;
+        static public int org_patient_number = 100;
+
+        static public int in_hospital_sum = 0;
+
+        static void Main(string[] args)
+        {
+            //-----コマンドライン処理-------------
+            string[] cmds = System.Environment.GetCommandLineArgs();
+
+            //パラメータ識別
+            for (int i = 0; i < cmds.Length; i++)
+            {
+                if (cmds[i].Trim() == "/org_num")
+                {
+
+                    if (cmds.Length - 1 >= i + 1) //パラメータが存在するなら
+                        org_patient_number = int.Parse(cmds[i + 1].Trim());
+
+                    Console.WriteLine("Org. patient number: " + org_patient_number.ToString());
+
+                }
+                else if (cmds[i].Trim() == "/bed")
+                {
+                    if (cmds.Length - 1 >= i + 1) //パラメータが存在するなら
+                        g.bed_count = int.Parse(cmds[i + 1].Trim());
+
+                    Console.WriteLine("Hostipal bed count: " + g.bed_count.ToString());
+
+
+                }
+                else if (cmds[i].Trim() == "/dead_rate")
+                {
+                    if (cmds.Length - 1 >= i + 1) //パラメータが存在するなら
+                        c.dead_rate = double.Parse(cmds[i + 1].Trim());
+
+                    Console.WriteLine("Dead Rate: " + c.dead_rate);
+                }
+
+            }
+            //-----コマンドライン処理-------------
+
+            for (int i = 0; i < org_patient_number; i++)
+            {
+                CoronaPatient new_p = new CoronaPatient(r,c);
+                new_p.id = p_list.Count;
+                new_p.family_id = p_list.Count;
+                new_p.care_level = 1; //感染されたばかりではなにもしない
+                new_p.serious_factor = 0.1; //発症したばかりではひどくない
+                p_list.Add(new_p);
+            }
+
+            Console.WriteLine("DAY:" + day.ToString() + " patient_number:" + p_list.Count);
+            Console.WriteLine(" complete_cured:" + complete_cure_count.ToString() + " dead:" + dead_count.ToString());
+
+
+            for (int j = 0; j < 100; j++)
+            {
+                int today_infect_sum = 0;
+                int today_mask_sum = 0;
+                int today_mask_predict = (int)(yesterday_mask_sum * mask_step_rate);
+                double r0_mean = 0;
+                int all_infected_number = 0;
+                int care_level_4_count = 0;
+                
+
+                for (int i = 0; i < p_list.Count; i++)
+                {
+
+                    if (p_list[i].dead == 0 && p_list[i].complete_cure == 0)
+                    {
+                        List<CoronaPatient> new_list = p_list[i].infect(r, c);
+                        dead_count += p_list[i].sick_step(r, c);
+                        complete_cure_count += p_list[i].complete_cure_step();
+                        today_infect_sum += p_list[i].use_mask;
+                        p_list[i].contact_number = (int)(p_list[i].contact_number * contact_step_rate);
+
+                        if (p_list[i].care_level >= 4)
+                            care_level_4_count += 1; 
+
+                        if (new_list.Count > 0)
+                        {
+                            today_infect_sum += new_list.Count;
+                            //Console.WriteLine("ID:" + i.ToString() + " infected " + new_list.Count.ToString() + " peoples");
+                            p_list.AddRange(new_list);
+                        }
+                    }
+
+
+
+                    if (i < 100) //r0は死亡と関係ない
+                        all_infected_number += p_list[i].infected_number;
+
+                    r0_mean =(double)all_infected_number / 100;
+                }
+
+                if (care_level_4_count > g.bed_count)
+                {
+                    c.org_dead_rate = c.dead_rate;
+                    c.dead_rate = c.dead_rate_no_bed;
+
+                }
+                else
+                {
+                    c.dead_rate = c.org_dead_rate;
+                }
+
+                in_hospital_sum = care_level_4_count;
+
+                if (in_hospital_sum > g.bed_count)
+                    in_hospital_sum = g.bed_count;
+
+                c.left_bed_count = g.bed_count - in_hospital_sum;
+
+                yesterday_mask_sum = today_mask_sum;
+
+                day++;
+                Console.WriteLine("---");
+                Console.WriteLine("DAY:" + day.ToString() + " patient_number:" + p_list.Count + " in_hospital:" + in_hospital_sum.ToString());
+                Console.WriteLine("infected:" + today_infect_sum.ToString() + " R0_mean:" + r0_mean.ToString("N2") + " cured:" + complete_cure_count.ToString() + " dead:" + dead_count.ToString());
+
+                //list_patient(0, p_list.Count - 1);
+                Console.ReadKey();
+            }
+
+        }
+
+        static void list_patient(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.WriteLine("---");
+                Console.WriteLine("ID:" + p_list[i].id.ToString() + " AGE:" + p_list[i].age.ToString() + " SEX:" + p_list[i].gender.ToString() + " FAMILY_NUM:" + p_list[i].family_number.ToString());
+                Console.WriteLine("CHECK:" + p_list[i].checkup.ToString() + " SERIOUS_FACTOR:" + p_list[i].serious_factor.ToString("N2") + " HEALTH_FACTOR:" + p_list[i].health_factor.ToString("N2"));
+            }
+        }
+    }
+}
